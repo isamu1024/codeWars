82e5e25c5ac9364a402e1d9ddc078055e23d6308
Alexandre Moro <alexandre.moro@outlook.com>
2020-04-09 21:06:28 +0200

    Working proposal

--- typeScript/DigitalRoot/DigitalRoot.ts
+++ typeScript/DigitalRoot/DigitalRoot.ts
@@ -1,20 +1,22 @@
-export const digitalRoot = (n:number):number => {
-    
 
-  };
+const digitalRoot = (n:number):number => {
 
-/**
- * 
- * const test: number = 1234;
-const test2: number = 234;
-const test3: number = 34;
-const test4: number = 4;
+if (n <= 9)
+  return n;
 
-console.log((test / (1* Math.pow(10,(test.toString().length-1)))).toFixed();
-console.log((test2 / (1 * Math.pow(10, (test2.toString().length - 1)))).toFixed());
-console.log((test3 / (1 * Math.pow(10, (test3.toString().length - 1)))).toFixed());
-console.log((test4 / (1 * Math.pow(10, (test4.toString().length - 1)))).toFixed());
- * 
- * 
- * 
- */
+let nString: string = String(n);
+let nArray : number[] = [];
+let calc: number = 0;
+
+for (let i:number = 0; i < n.toString().length; i++) {
+  nArray.push(Number(nString.charAt(i)));
+  calc += nArray[i];
+}
+
+return digitalRoot(calc);
+
+};
+
+
+
+console.log(digitalRoot(132189));
