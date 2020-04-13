a5e63cf123b3b016a185f4b705fc6c70cec16b36
isamu1024 <alexandre.moro@outlook.com>
2020-04-13 18:24:22 +0200

    dirty but working

--- typeScript/PrimesInNumbers/PrimeInNumbers.ts
+++ typeScript/PrimesInNumbers/PrimeInNumbers.ts
@@ -1,6 +1,7 @@
-function div(n: number): Number[] {
+function div(n: number): string {
   let i: number = 2;
   let fact: number[] = [];
+  let output: string = ``;
 
   let div = (x: number) => {
     i = 2;
@@ -15,7 +16,21 @@ function div(n: number): Number[] {
     n = n / i;
   }
 
-  return fact;
+  for (let i = 0; i < fact.length; i++) {
+    if (fact.length == 1 || fact[i] != fact[i + 1]) {
+      output += `(${fact[i]})`;
+    } else {
+      let n: number = i;
+      let count: number = 1;
+      while (fact[n] == fact[n + 1]) {
+        count++;
+        n++;
+      }
+      output += `(${fact[i]}**${count})`;
+    }
+  }
+
+  return output;
 }
 
-let value = div(7775460);
+console.log(div(7775460));
