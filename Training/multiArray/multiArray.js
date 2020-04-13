b62745e421148da2caf39b763c56bfbb75ef560e
isamu1024 <alexandre.moro@outlook.com>
2020-04-13 21:07:32 +0200

    just testing

---
+++ Training/multiArray/multiArray.js
@@ -0,0 +1,14 @@
+var arrayM = [];
+arrayM.push([, 9]);
+console.log(arrayM[0][1]);
+debugger;
+var multi = [
+  [1, 2, 3],
+  [23, 24, 25],
+];
+console.log(multi[0][0]);
+console.log(multi[0][1]);
+console.log(multi[0][2]);
+console.log(multi[1][0]);
+console.log(multi[1][1]);
+console.log(multi[1][2]);
---
+++ Training/multiArray/multiArray.ts
@@ -0,0 +1,16 @@
+let arrayM: number[][] = [];
+
+arrayM.push([0, 9]);
+console.log(arrayM[0][0]);
+debugger;
+
+let multi: number[][] = [
+  [1, 2, 3],
+  [23, 24, 25],
+];
+console.log(multi[0][0]);
+console.log(multi[0][1]);
+console.log(multi[0][2]);
+console.log(multi[1][0]);
+console.log(multi[1][1]);
+console.log(multi[1][2]);
