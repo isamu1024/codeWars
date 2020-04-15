1426d8bb5e4c6ece43350fe47a22ce1eec33a0d2
isamu1024 <alexandre.moro@outlook.com>
2020-04-15 23:17:32 +0200

    final submit

--- typeScript/DuplicateEncoder/DuplicateEncoder.ts
+++ typeScript/DuplicateEncoder/DuplicateEncoder.ts
@@ -2,11 +2,11 @@ export function duplicateEncode(word: string) {
   let isPresent = (c: string[], w: string): string => {
     return c.lastIndexOf(w) != c.indexOf(w) ? `)` : `(`;
   };
-  let wordArray: string[] = word.split(``);
+  let wordArray: string[] = word.toLowerCase().split(``);
   let output: string = ``;
 
   wordArray.forEach((element) => {
-    output += isPresent(word.split(``), element);
+    output += isPresent(wordArray, element);
   });
 
   return output;
