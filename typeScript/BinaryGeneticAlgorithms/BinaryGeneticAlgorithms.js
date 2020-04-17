663b07362f0fd3a4f20b7b73077ca8fa77aa5cfd
Alexandre Moro <alexandre.moro@outlook.com>
2020-04-17 09:58:49 +0200

    wip

--- typeScript/BinaryGeneticAlgorithms/BinaryGeneticAlgorithms.js
+++ typeScript/BinaryGeneticAlgorithms/BinaryGeneticAlgorithms.js
@@ -1,42 +1,64 @@
 "use strict";
 exports.__esModule = true;
 var GeneticAlgorithm = /** @class */ (function () {
-    function GeneticAlgorithm() {
-        this.population = [];
-        this.fitnesses = [];
-    }
-    GeneticAlgorithm.prototype.generate = function (length) {
-        function getRandombin() {
-            return Math.floor(Math.random() * 2);
-        }
-        //setting up array for chromosome and associated fitnesses
-        debugger;
-        //generating population
-        while (this.population.length <= 99) {
-            var chromosome = "";
-            for (var i = 0; i < length; i++) {
-                chromosome += getRandombin().toString();
-            }
-            this.population.push(chromosome);
-            this.fitnesses.push(0);
-        }
-        debugger;
-    };
-    GeneticAlgorithm.prototype.select = function (population, fitnesses) {
-        // TODO: Implement the select method
-    };
-    GeneticAlgorithm.prototype.mutate = function (chromosome, p) {
-        // TODO: Implement the mutate method
-    };
-    GeneticAlgorithm.prototype.crossover = function (chromosome1, chromosome2) {
-        // TODO: Implement the crossover method
-    };
-    GeneticAlgorithm.prototype.run = function (fitness, length, p_c, p_m, iterations) {
-        if (iterations === void 0) { iterations = 100; }
-        // TODO: Implement the run method
-    };
-    return GeneticAlgorithm;
-}());
+	function GeneticAlgorithm() {
+		this.population = [];
+		this.fitnesses = [];
+	}
+	GeneticAlgorithm.prototype.getRandomInt = function (max) {
+		return Math.floor(Math.random() * max);
+	};
+	GeneticAlgorithm.prototype.generate = function (length) {
+		//setting up array for chromosome and associated fitnesses
+		//generating population
+		while (this.population.length <= 4) {
+			var chromosome = "";
+			for (var i = 0; i < length; i++) {
+				chromosome += this.getRandomInt(2).toString();
+			}
+			this.population.push(chromosome);
+			this.fitnesses.push(0);
+		}
+	};
+	GeneticAlgorithm.prototype.select = function (population, fitnesses) {
+		// TODO: Implement the select method
+	};
+	GeneticAlgorithm.prototype.mutate = function (chromosome, p) {
+		// TODO: Implement the mutate method
+	};
+	GeneticAlgorithm.prototype.crossover = function (chromosome1, chromosome2) {
+		var randomCut = this.getRandomInt(chromosome1.length - 1);
+		var newChromosome1 =
+			chromosome1.slice(0, randomCut) +
+			chromosome2.slice(randomCut, chromosome2.length);
+		var newChromosome2 =
+			chromosome1.slice(randomCut, chromosome1.length) +
+			chromosome2.slice(0, randomCut);
+		var index1 = this.population.indexOf(chromosome1);
+		var index2 = this.population.indexOf(chromosome2);
+		debugger;
+		this.population[index1] = newChromosome1;
+		this.population[index2] = newChromosome2;
+		this.fitnesses[index1] = 0;
+		this.fitnesses[index2] = 0;
+		debugger;
+	};
+
+	GeneticAlgorithm.prototype.run = function (
+		fitness,
+		length,
+		p_c,
+		p_m,
+		iterations
+	) {
+		if (iterations === void 0) {
+			iterations = 100;
+		}
+		// TODO: Implement the run method
+	};
+	return GeneticAlgorithm;
+})();
 exports.GeneticAlgorithm = GeneticAlgorithm;
 var test = new GeneticAlgorithm();
-test.generate(40);
+test.generate(8);
+test.crossover(test.population[0], test.population[1]);
--- typeScript/BinaryGeneticAlgorithms/BinaryGeneticAlgorithms.ts
+++ typeScript/BinaryGeneticAlgorithms/BinaryGeneticAlgorithms.ts
@@ -1,50 +1,67 @@
 export class GeneticAlgorithm {
-  population: string[] = [];
-  fitnesses: number[] = [];
+	population: string[] = [];
+	fitnesses: number[] = [];
 
-  generate(length: number) {
-    function getRandombin() {
-      return Math.floor(Math.random() * 2);
-    }
+	getRandomInt(max: number) {
+		return Math.floor(Math.random() * max);
+	}
 
-    //setting up array for chromosome and associated fitnesses
+	generate(length: number) {
+		//setting up array for chromosome and associated fitnesses
+		//generating population
 
-    debugger;
-    //generating population
-    while (this.population.length <= 99) {
-      let chromosome: string = ``;
-      for (let i = 0; i < length; i++) {
-        chromosome += getRandombin().toString();
-      }
-      this.population.push(chromosome);
-      this.fitnesses.push(0);
-    }
+		while (this.population.length <= 4) {
+			let chromosome: string = ``;
+			for (let i = 0; i < length; i++) {
+				chromosome += this.getRandomInt(2).toString();
+			}
+			this.population.push(chromosome);
+			this.fitnesses.push(0);
+		}
+	}
 
-    debugger;
-  }
+	select(population: string[], fitnesses: number[]) {
+		// TODO: Implement the select method
+	}
 
-  select(population: string[], fitnesses: number[]) {
-    // TODO: Implement the select method
-  }
+	mutate(chromosome: string, p: number) {
+		// TODO: Implement the mutate method
+	}
 
-  mutate(chromosome: string, p: number) {
-    // TODO: Implement the mutate method
-  }
+	crossover(chromosome1: string, chromosome2: string) {
+		// we cut off the rest of the chromosome and switch it with the cut off part of the other one.
+		//In other words, say we have 01011010 and 11110110 and we crossover at the 3rd bit. This results in 010 10110 and 111 11010
 
-  crossover(chromosome1: string, chromosome2: string) {
-    // TODO: Implement the crossover method
-  }
+		let randomCut: number = this.getRandomInt(chromosome1.length - 1);
 
-  run(
-    fitness: (chromosome: string) => number,
-    length: number,
-    p_c: number,
-    p_m: number,
-    iterations = 100
-  ) {
-    // TODO: Implement the run method
-  }
+		let newChromosome1 =
+			chromosome1.slice(0, randomCut) +
+			chromosome2.slice(randomCut, chromosome2.length);
+		let newChromosome2 =
+			chromosome1.slice(randomCut, chromosome1.length) +
+			chromosome2.slice(0, randomCut);
+
+		let index1: number = this.population.indexOf(chromosome1);
+		let index2: number = this.population.indexOf(chromosome2);
+
+		this.population[index1] = newChromosome1;
+		this.population[index2] = newChromosome2;
+
+		this.fitnesses[index1] = 0;
+		this.fitnesses[index2] = 0;
+	}
+
+	run(
+		fitness: (chromosome: string) => number,
+		length: number,
+		p_c: number,
+		p_m: number,
+		iterations = 100
+	) {
+		// TODO: Implement the run method
+	}
 }
 
 let test = new GeneticAlgorithm();
-test.generate(40);
+test.generate(8);
+test.crossover(test.population[0], test.population[1]);
