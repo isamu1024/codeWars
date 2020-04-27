95d8c0d70b281671427e527dac6bcd197fa3d529
Alexandre Moro <alexandre.moro@outlook.com>
2020-04-27 19:12:57 +0200

    wip

--- typeScript/LinearEquationSolver/LinearEquationSolver.js
+++ typeScript/LinearEquationSolver/LinearEquationSolver.js
@@ -1,18 +1,35 @@
 function splitEq(equation) {
-  var re = new RegExp(/(-?\d?x)|(-?\d?y)|(-?\d?z)|=|\+|\d+/g);
-  return equation.match(re);
+    var re = new RegExp(/(-?\d?x)|(-?\d?y)|(-?\d?z)|=|\+|\d+/g);
+    return equation.match(re);
 }
 function splitEqual(equation) {
-  var splitEqual = equation.split("=");
-  splitEqual[1] = splitEqual[1]
-    .split("")
-    .map(function (c) {
-      return c.replace("-", "+");
+    var re = new RegExp(/(-?\d?x)|(-?\d?y)|(-?\d?z)/);
+    var splitEqual = equation.split("=");
+    splitEqual[0] = splitEqual[0].split(/\+/).join("");
+    splitEqual[1] = splitEqual[1]
+        .split("")
+        .map(function (c) {
+        if (c == "-") {
+            return c.replace("-", "");
+        }
+        else if (c == "+") {
+            return c.replace("+", "-");
+        }
+        else {
+            return c;
+        }
     })
-    .join("");
-  splitEqual[0].push(splitEqual[1].pop());
-  debugger;
-  equation = splitEqual.join("");
+        .join("");
+    equation = splitEqual.concat().join("");
+    debugger;
+    return equation;
 }
-console.log(splitEq("2x+8y=-2z-4"));
-splitEqual("2x+8y=-2z-4");
+function matrix(equation) {
+    var re = new RegExp(/\d?x/g);
+    var aX = [];
+    aX.push(equation.match(re));
+    debugger;
+}
+//console.log(splitEq(`2x+8y=-2z+4`));
+//console.log(splitEqual(`2x+8y=0`));
+matrix("2x8y4x0");
--- typeScript/LinearEquationSolver/LinearEquationSolver.ts
+++ typeScript/LinearEquationSolver/LinearEquationSolver.ts
@@ -1,19 +1,44 @@
 function splitEq(equation: string) {
-  let re = new RegExp(/(-?\d?x)|(-?\d?y)|(-?\d?z)|=|\+|\d+/g);
+	let re = new RegExp(/(-?\d?x)|(-?\d?y)|(-?\d?z)|=|\+|\d+/g);
 
-  return equation.match(re);
+	return equation.match(re);
 }
 
 function splitEqual(equation: string) {
-  let re = new RegExp(/(-?\d?x)|(-?\d?y)|(-?\d?z)/g);
+	let re = new RegExp(/(-?\d?x)|(-?\d?y)|(-?\d?z)/);
 
-  let splitEqual: string[] = equation.split("=");
-  splitEqual[1] = splitEqual[1]
-    .split(``)
-    .map((c) => c.replace(`-`, `+`))
-    .join(``);
-  debugger;
+	let splitEqual: string[] = equation.split("=");
+	splitEqual[0] = splitEqual[0].split(/\+/).join(``);
+	splitEqual[1] = splitEqual[1]
+		.split(``)
+		.map((c) => {
+			if (c == `-`) {
+				return c.replace(`-`, ``);
+			} else if (c == `+`) {
+				return c.replace(`+`, `-`);
+			} else {
+				return c;
+			}
+		})
+		.join(``);
+
+	equation = splitEqual.concat().join(``);
+	debugger;
+	return equation;
 }
 
-console.log(splitEq(`2x+8y=-2z-4`));
-splitEqual(`2x+8y=-2z-4`);
+function matrix(equation: string) {
+	let reX = new RegExp(/\d?x/g);
+	let reY = new RegExp(/\d?y/g);
+	let reZ = new RegExp(/\d?z/g);
+	let aX = [];
+
+	aX.push(equation.match(reX));
+
+	debugger;
+}
+
+//console.log(splitEq(`2x+8y=-2z+4`));
+//console.log(splitEqual(`2x+8y=0`));
+
+matrix(`2x8y4x0`);
