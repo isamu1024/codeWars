03dded6040f2562b2aa316c9ee9a51f075e122db
Alexandre Moro <alexandre.moro@outlook.com>
2020-05-11 02:32:57 +0200

    fonctionnel mais lent

--- typeScript/RectangleRotation/rectangleRotation.ts
+++ typeScript/RectangleRotation/rectangleRotation.ts
@@ -1,1 +1,23 @@
-export function rectangleRotation(a: number, b: number) {}
+export function rectangleRotation(a: number, b: number) {
+  let angle: number = (45 * Math.PI) / 180;
+  let count: number = 0;
+  let max = Math.floor(
+    Math.abs((-a / 2) * Math.cos(angle) - (b / 2) * Math.sin(angle))
+  );
+
+  for (let x: number = 0; x <= max; x++) {
+    for (let y: number = -max; y <= max; y++) {
+      let rotateX: number = x * Math.cos(angle) - y * Math.sin(angle);
+      let rotateY: number = y * Math.cos(angle) + x * Math.sin(angle);
+      if (
+        rotateX >= -a / 2 &&
+        rotateX <= a / 2 &&
+        rotateY >= -b / 2 &&
+        rotateY <= b / 2
+      ) {
+        count++;
+      }
+    }
+  }
+  return count * 2 - 3;
+}
