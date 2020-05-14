8617a38b8f44b3ca1ff0a1d21df4334d151d8f26
Alexandre Moro <alexandre.moro@outlook.com>
2020-05-15 00:09:38 +0200

    Marchote, il me faut rajouter un simplification automatique

--- typeScript/CommonDenominators/CommonDenominator.ts
+++ typeScript/CommonDenominators/CommonDenominator.ts
@@ -1,25 +1,35 @@
-export const convertFrac = (lst: [number, number][]) => {
-  let pgcd = (a: any, b: any): any => {
-    let r: number;
-    a > b ? (r = a % b) : (r = b % a);
-    return r === 0 ? b : pgcd(b, r);
-  };
+export const convertFrac = (lst: [number, number][]): string => {
+	let str: string = ``;
 
-  let denArray: any = lst.concat
-    .apply([], lst)
-    .filter((v, i) => {
-      if (i % 2 !== 0) {
-        return v;
-      }
-    });
+	let pgcd = (a: any, b: any): any => {
+		let r: number;
+		a > b ? (r = a % b) : (r = b % a);
+		return r === 0 ? b : pgcd(b, r);
+	};
 
-  denArray.reduce((acc, val, idx) =>  );
+	let denArray: any = lst.concat.apply([], lst).filter((v, i) => {
+		if (i % 2 !== 0) {
+			return v;
+		}
+	});
 
-  debugger;
+	let ppcm: number = denArray.reduce(
+		(a: number, b: number) => (a * b) / pgcd(a, b)
+	);
+
+	let multiply: any = lst.map((x) => x.map((val, idx) => val * (ppcm / x[1])));
+
+	multiply.forEach((element: any) => {
+		str += `(${element[0]},${element[1]})`;
+	});
+
+	return str;
 };
 
-let test = convertFrac([
-  [1, 2],
-  [1, 3],
-  [1, 4],
-]);
+console.log(
+	convertFrac([
+		[69, 130],
+		[87, 1310],
+		[3, 4],
+	])
+);
--- typeScript/CommonDenominators/PGCD.ts
+++ typeScript/CommonDenominators/PGCD.ts
@@ -1,12 +1,12 @@
 function pgcd(a: number, b: number): number {
-  let r: number;
+	let r: number;
 
-  a > b ? (r = a % b) : (r = b % a);
-  return r === 0 ? b : pgcd(b, r);
+	a > b ? (r = a % b) : (r = b % a);
+	return r === 0 ? b : pgcd(b, r);
 }
 
-let lst: number[] = [2, 3, 4];
+let lst: number[] = [2, 5, 4, 9, 10];
 
-let ppcm: number = lst.reduce((a, b) => (a * b) / pgcd(a, b));
+let pgcd_: number = lst.reduce((a, b) => pgcd(b, a));
 
 debugger;
