e1d71f2a7b1b99a54dd9d3657529d4e1cd831810
Alexandre Moro <alexandre.moro@outlook.com>
2020-05-15 11:59:15 +0200

    Fonctionne mais nécessite de corriger la conversion de type.

--- typeScript/CommonDenominators/CommonDenominator.ts
+++ typeScript/CommonDenominators/CommonDenominator.ts
@@ -1,36 +1,47 @@
 export const convertFrac = (lst: [number, number][]): string => {
-	let str: string = ``;
+  let str: string = ``;
 
-	if (lst.length === 0) return str;
+  if (lst.length === 0) return str;
 
-	let pgcd = (a: any, b: any): any => {
-		let r: number;
-		a > b ? (r = a % b) : (r = b % a);
-		return r === 0 ? b : pgcd(b, r);
-	};
+  let pgcd = (a: any, b: any): any => {
+    let r: number;
+    a > b ? (r = a % b) : (r = b % a);
+    return r === 0 ? b : pgcd(b, r);
+  };
 
-	let denArray: any = lst.concat.apply([], lst).filter((v, i) => {
-		if (i % 2 !== 0) {
-			return v;
-		}
-	});
+  let simpLst = lst.map((x) =>
+    x.map((val) =>
+      x[0] > x[1] ? val / pgcd(x[0], x[1]) : val / pgcd(x[1], x[0])
+    )
+  );
 
-	let ppcm: number = denArray.reduce((a: number, b: number) =>
-		a > b ? (a * b) / pgcd(a, b) : (a * b) / pgcd(b, a)
-	);
+  let denArray: any = simpLst.concat.apply([], simpLst).filter((v, i) => {
+    if (i % 2 !== 0) {
+      return v;
+    }
+  });
 
-	let multiply: any = lst.map((x) => x.map((val, idx) => val * (ppcm / x[1])));
+  let ppcm: number = denArray.reduce((a: number, b: number) =>
+    a > b ? (a * b) / pgcd(a, b) : (a * b) / pgcd(b, a)
+  );
 
-	multiply.forEach((element: any) => {
-		str += `(${element[0]},${element[1]})`;
-	});
+  let multiply: any = simpLst.map((x) =>
+    x.map((val, idx) => val * (ppcm / x[1]))
+  );
 
-	return str;
+  multiply.forEach((element: any) => {
+    str += `(${element[0]},${element[1]})`;
+  });
+
+  return str;
 };
 
 console.log(
-	convertFrac([
-		[2, 3],
-		[8, 9],
-	])
+  convertFrac([
+    [1, 2],
+    [4, 5],
+    [3, 4],
+    [2, 3],
+    [7, 10],
+  ])
 );
