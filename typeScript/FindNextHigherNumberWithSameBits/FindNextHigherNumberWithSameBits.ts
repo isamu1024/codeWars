e647ccecbbc3e2a206578a897ac6eca5ef10891e
Alexandre Moro <alexandre.moro@outlook.com>
2020-05-30 00:48:28 +0200

    wip

--- typeScript/FindNextHigherNumberWithSameBits/FindNextHigherNumberWithSameBits.ts
+++ typeScript/FindNextHigherNumberWithSameBits/FindNextHigherNumberWithSameBits.ts
@@ -1,27 +1,40 @@
 function nextHigher(n: number): number {
-	let binArray: number[] = n
-		.toString(2)
-		.split(``)
-		.map((x) => Number(x));
+  //convert number to binary array
+  let binArray: number[] = n
+    .toString(2)
+    .split(``)
+    .map((x) => Number(x))
+    .reverse();
 
-	binArray.unshift(0);
+  binArray.push(0);
 
-	debugger;
+  debugger;
 
-	let cursor: [number, number] = [Number.MAX_VALUE, Number.MAX_VALUE];
+  //const posZero: number[] = [];
 
-	binArray.map((val, idx, arr) =>
-		arr[idx] === 0 && arr[idx + 1] === 1 ? (cursor[0] = idx) : false
-	);
+  //   for (let i = 0; i <= binArray.length; i++) {
+  //     if (binArray[i] === 0) posZero.push(i);
+  //   }
 
-	return binArray
-		.map((val, idx, arr) => {
-			if (idx === cursor[0]) return 1;
-			if (idx === cursor[0] + 1) return 0;
-			return val;
-		})
-		.reverse()
-		.reduce((prev, curr, idx) => curr * Math.pow(2, idx) + prev);
+  //   let posOne: number[] = [];
+
+  //   for (let i = 0; i <= binArray.length; i++) {
+  //     if (binArray[i] === 1) posOne.push(i);
+  //   }
+
+  let test: number = n;
+
+  if (Math.pow(binArray.lastIndexOf(0), 2) < Math.pow(binArray.indexOf(1), 2)) {
+    test =
+      n - Math.pow(binArray.lastIndexOf(1), 2) + (binArray.lastIndexOf(0), 2);
+  }
+
+  debugger;
+
+  //swap
+
+  //convert binary array to number
+  return binArray.reduce((prev, curr, idx) => curr * Math.pow(2, idx) + prev);
 }
 
-console.log(nextHigher(1022));
+console.log(nextHigher(127));
